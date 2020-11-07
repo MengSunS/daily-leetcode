6917a216f0c68a978bfd6acfa47c9012605b9ac9
Pokemonzz <sunx0585@umn.edu>
2020-11-07 00:30:34 -0800

    d, left (right: left+d), k

--- lc_1039_minimumScoreTriangulation.py
+++ lc_1039_minimumScoreTriangulation.py
@@ -1,3 +1,21 @@
+-------思维1： 按照多边形边数bottom up 轮回切pizza -----
+
+class Solution:
+    def minScoreTriangulation(self, A: List[int]) -> int:
+        n= len(A)
+        dp= [[0]*n for _ in range(n)]
+        
+        for d in range(3, n+1):
+            for i in range(0, n+1-d):
+                j= i+ d-1
+                dp[i][j]= sys.maxsize
+                for k in range(i+1, j):
+                    dp[i][j]= min(dp[i][j], dp[i][k]+ dp[k][j]+ A[i]*A[j]*A[k])
+        return dp[0][n-1]
+        
+
+------思维2： 按照右边坐标退回去切pizza ---------
+
 class Solution:
     def minScoreTriangulation(self, A: List[int]) -> int:
         dp= [[0]*len(A) for _ in range(len(A))]
--- lc_312_Burst_Ballons.py
+++ lc_312_Burst_Ballons.py
@@ -1,3 +1,23 @@
+-----思路1：先fix区间长度--------
+
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        nums= [1]+ nums+ [1]
+        n= len(nums)
+        dp= [[0]*n for _ in range(n)]
+        
+        for d in range(2, n):
+            for i in range(0, n-d):
+                j= i+d
+                for k in range(i+1, j):
+                    dp[i][j]= max(dp[i][j], dp[i][k]+ dp[k][j]+ nums[k]* nums[i]* nums[j])
+                    
+        return dp[0][n-1]
+                
+
+
+-----思路2：----------
+
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
         nums= [1]+ nums+ [1]
