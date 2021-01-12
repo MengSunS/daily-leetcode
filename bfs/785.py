0ede4f3220af972f0b2fc433f0b210705de054a7
Pokemonzz <sunx0585@umn.edu>
2021-01-12 00:04:59 -0800

    判断visited放在neighbors for loop里面比较快

--- bfs/785.py
+++ bfs/785.py
@@ -1,3 +1,5 @@
+# Method1: bfs
+
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         seen= {}
@@ -18,3 +20,31 @@ class Solution:
                         q.append((nxt, -group))        
         return True 
 
+#Method2: dfs
+
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        seen= {}
+        # for i loop是因为可能不是连通图，非连通图每个子图都得是二分图
+        for i in range(len(graph)):
+            if i in seen:
+                continue     
+            
+            res= self.dfs(graph, seen, i, 1)
+            if not res:
+                return False
+        return True
+    
+    def dfs(self, graph, seen, node, group):
+        
+        for nxt in graph[node]:
+            if nxt in seen and seen[nxt]== group:
+                return False
+            elif nxt not in seen:
+                seen[nxt]= -group
+                res= self.dfs(graph, seen, nxt, -group) #dfs return value的内部recursion要用var接着
+                if res== False:
+                    return False
+        return True
+            
+        
