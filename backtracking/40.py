1c4a433300ec4ecaaaa6b3af49c1e524c14af684
Pokemonzz <sunx0585@umn.edu>
2021-02-09 22:47:56 -0800

    if cur> targe: break pruning

--- backtracking/40.py
+++ backtracking/40.py
@@ -1,29 +1,24 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         
-        def dfs(startIndex, target, path, res, visited):
+        def dfs(startIndex, target, path, res):
             if target== 0:
-                res.append(path[:])
+                res.append(path)
                 return 
+            
             if target< 0:
                 return 
+            
             for i in range(startIndex, len(candidates)):
-                if i in visited:
-                    continue
-                if i>= 1 and candidates[i]== candidates[i-1] and i-1 not in visited:
-                    continue
-                visited.add(i)
-                dfs(i+ 1, target- candidates[i], path+ [candidates[i]], res, visited)
-                visited.remove(i)
-                
-        candidates= sorted(candidates)
-        res= []
-        visited= set()
-        dfs(0, target, [], res, visited)
-        return res
-        
-                
+                if candidates[i]> target:
+                    break
                     
+                if i> startIndex and candidates[i]== candidates[i-1]:
+                    continue
+                    
+                dfs(i+ 1, target- candidates[i], path+ [candidates[i]], res)
                 
-        
-        
+        candidates.sort()
+        res= []
+        dfs(0, target, [], res)
+        return res
