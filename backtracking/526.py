04574a96ec2823252d6f8270f01b765e2c87d331
Pokemonzz <sunx0585@umn.edu>
2021-04-25 15:25:15 -0700

    Beautiful Arrangement 竟然可以加memo

--- backtracking/526.py
+++ backtracking/526.py
@@ -1,3 +1,5 @@
+# Method 1: 比brute force略微好一点，没直接放进去再检测(O(n!)).这种先检测及时止损O(k), k是所有合格permutation个数 
+
 class Solution:
     def countArrangement(self, n: int) -> int:
         def dfs(visited, pos):
@@ -17,6 +19,32 @@ class Solution:
         visited= [0]* (n+1)
         dfs(visited, 1)
         return self.res
+
+# Method 2: 其实可以放个memo, 大大提升，这个方法是在复习时评论区里的
+
+# 加memo的很不擅长，纯recursion的也不擅长。这题的memo记得是avaialbe set X 有几种方法
+#因为btr不是同时进行的，是完成一个深度所有，再另一个分支。前一个到底的已完成了它路上的所有分支，
+# 对于与后一个大分支同parent node的下面的是一样的
+
+class Solution:
+    def countArrangement(self, N: int) -> int:
+        
+        def dfs(X):
+            if len(X) == 1:
+                return 1
+            
+            if X in cache:
+                return cache[X]
+            total = 0
+            for x in X:
+                if x % len(X) == 0 or len(X) % x == 0:
+                    total += dfs(X - {x})
+                    
+            cache[X] = total 
+            return total 
+        
+        cache = {}
+        return dfs(frozenset(range(1, N+1)))
     
     
     
