b0fa0d7c19cc3f16c24dd52963d03352ee8566a8
Pokemonzz <sunx0585@umn.edu>
2021-05-03 22:03:09 -0700

    Count Complete Tree Nodes

--- binary_tree/tree/222.py
+++ binary_tree/tree/222.py
@@ -1,3 +1,27 @@
+# method 1: take advantage of complete tree, O(logN ^ 2)
+
+# time: O(logN ^ 2). 每层2**(d - 1), 前d层总数2**d-1 
+class Solution:
+    def countNodes(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        lh, rh = self.getH(root.left), self.getH(root.right)
+        if lh == rh: #左子树一定满, 2**lh-1为左子树上的个数，+1为root
+            return pow(2, lh) - 1 + 1 + self.countNodes(root.right)
+        else: #右子树一定满
+            return pow(2, rh) - 1 + 1 + self.countNodes(root.left)
+    
+    def getH(self, node):  
+        ret = 0
+        while node:
+            ret += 1
+            node = node.left
+        return ret
+
+# method 2: worse performace, linear time
+
+
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
