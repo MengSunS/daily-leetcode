b9a14d0464bd2485456f03bc06139ab0c54ac01b
Pokemonzz <sunx0585@umn.edu>
2021-05-05 21:55:34 -0700

    Subtree of Another Tree

--- binary_tree/572.py
+++ binary_tree/572.py
@@ -1,27 +1,12 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def isSubtree(self, r: TreeNode, s: TreeNode) -> bool:
-        if not r: return False
-        if r.val == s.val and self.check(r, s):
+        if not r:
+            return 
+        if r.val == s.val and self.match(r, s):
             return True
         return self.isSubtree(r.left, s) or self.isSubtree(r.right, s)
-    def check(self, r, s):
-            if not r and not s:
-                return True
-            if (not r and s) or (r and not s):
-                return False
-            
-            if r.val != s.val:
-                return False
-            return self.check(r.left, s.left) and self.check(r.right, s.right)
-        
-
-        
-        
-            
-        
+    
+    def match(self, r, s):
+        if r and s:
+            return r.val == s.val and self.match(r.left, s.left) and self.match(r.right, s.right)
+        return r == s
