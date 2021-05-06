8b4567070eefd0f10a5eb7737f76021fe0768b5b
Pokemonzz <sunx0585@umn.edu>
2021-05-05 18:29:26 -0700

    Invert Binary Tree

--- binary_tree/226.py
+++ binary_tree/226.py
@@ -1,3 +1,19 @@
+#root.left指向root.right 子树翻转完的子根节点，返回的是root
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)     
+            return root
+
+
+
+
+
+
+
+
+
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
