cf2859ac701fc91546e574c064995fbd270c19e7
Pokemonzz <sunx0585@umn.edu>
2021-05-10 21:34:25 -0700

    Minimum Cost to Make at Least One Valid Path in a Grid

--- bfs/1368.py
+++ bfs/1368.py
@@ -1,3 +1,42 @@
+# lee215 
+
+class Solution:
+    def minCost(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        dp = [[float('inf')] * n for _ in range(m)]
+        dire = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        k = 0
+        bfs = []
+        def dfs(r, c):
+            if r >= m or r < 0 or c >= n or c < 0 or dp[r][c] != float('inf'):
+                return 
+            dp[r][c] = k
+            bfs.append((r, c))
+            dfs(r + dire[grid[r][c] - 1][0], c + dire[grid[r][c] - 1][1])
+        
+        dfs(0, 0)
+        while bfs:
+            k += 1
+            bfs, bfs2 = [], bfs
+            [dfs(x + dx, y + dy) for x, y in bfs2 for dx, dy in dire]
+        return dp[-1][-1]
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution:
     def minCost(self, grid: List[List[int]]) -> int:
         # bfs minimum layers, when layers are defined by number of changes
