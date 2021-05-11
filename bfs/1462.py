11430ff0c7798b5ffb8d6791f57db7ede2ba50cf
Pokemonzz <sunx0585@umn.edu>
2021-05-10 22:50:49 -0700

    Course Schedule IV

--- bfs/1462.py
+++ bfs/1462.py
@@ -1,4 +1,28 @@
+# course schedule系列，有向图，拓扑排序；此题如果每次查connect会超时，索性剥洋葱向下传递时把每个点的preSet joint一下
 class Solution:
+    def checkIfPrerequisite(self, n: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        graph = defaultdict(list)
+        preSet = defaultdict(set)
+        inDegree = {i: 0 for i in range(n)}
+        for x, y in prerequisites:
+            graph[x].append(y)
+            preSet[y].add(x)
+            inDegree[y] += 1
+        
+        bfs = deque([i for i in range(n) if not inDegree[i]])
+        while bfs:
+            cur = bfs.popleft()
+            for nxt in graph[cur]:
+                preSet[nxt] |= preSet[cur]
+                inDegree[nxt] -= 1
+                if not inDegree[nxt]:
+                    bfs.append(nxt)
+        
+        return [u in preSet[v] for u, v in queries]
+            
+        
+        
+        class Solution:
     def checkIfPrerequisite(self, n: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
         in_degree= {i:0 for i in range(n)}
         graph= {i: [] for i in range(n)}
