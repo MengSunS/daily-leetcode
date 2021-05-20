16d142d3c911859c07e42dda2c8226bf92b97998
Pokemonzz <sunx0585@umn.edu>
2021-05-20 11:47:45 -0700

    Minimum Remove to Make Valid Parentheses

---
+++ parentheses/1249.py
@@ -0,0 +1,18 @@
+# stack里放open brackets index. string is imutable, convert into list
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        stack = []
+        s= list(s)
+        for i, ch in enumerate(s):
+            if ch == '(':
+                stack.append(i)
+            elif ch == ')':
+                if stack:
+                    stack.pop()
+                else:
+                    s[i] = ''
+        while stack:
+            s[stack.pop()] = ''
+        return ''.join(s)
+            
+        
