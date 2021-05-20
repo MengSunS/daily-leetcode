d69110fc2a19d6e746231d317878822879f78899
Pokemonzz <sunx0585@umn.edu>
2021-05-20 00:41:09 -0700

    Different Ways to Add Parentheses

--- parentheses/241.py
+++ parentheses/241.py
@@ -1,3 +1,31 @@
+# 
+
+class Solution:
+    def diffWaysToCompute(self, s: str) -> List[int]:
+        @lru_cache(None)
+        def dfs(i, j):
+            if s[i: j + 1].isdigit():
+                return [int(s[i: j + 1])]
+            res = []
+            for k in range(i, j+1):
+                c = s[k]
+                if c in '*+-':
+                    l = dfs(i, k - 1)
+                    r = dfs(k + 1, j)
+                    res.extend([eval(str(x)+c+str(y)) for x in l for y in r]) #笛卡尔积
+            return res
+        
+        n = len(s)
+        return dfs(0, n - 1)
+
+
+
+
+
+
+
+
+
 class Solution:
     def diffWaysToCompute(self, expression: str) -> List[int]:
         m = {}
