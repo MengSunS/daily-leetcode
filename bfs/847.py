072f3a4cc288a0292bdd19b455dbdf134b27ace3
Pokemonzz <sunx0585@umn.edu>
2021-05-29 14:54:12 -0700

    Shortest Path Visiting All Nodes

--- bfs/847.py
+++ bfs/847.py
@@ -1,5 +1,31 @@
 class Solution:
     def shortestPathLength(self, graph: List[List[int]]) -> int:
+        n = len(graph)
+        target = (1 << n) - 1
+        q = [(i, 1 << i, 0) for i in range(n)]
+        seen = set([(i, 1 << i) for i in range(n)])
+        stack = []
+        while q:
+            for cur, state, steps in q:
+                for nxt in graph[cur]:
+                    nxt_state = state | (1 << nxt)
+                    if nxt_state == target:
+                        return steps + 1
+                    if (nxt, nxt_state) not in seen:
+                        seen.add((nxt, nxt_state))
+                        stack.append((nxt, nxt_state, steps + 1))
+            q, stack = stack, []
+        return 0
+        
+
+
+
+
+
+
+
+class Solution:
+    def shortestPathLength(self, graph: List[List[int]]) -> int:
         # (node, status after this node) 去重的是别的路线上比这个早的
         state= 0
         n= len(graph)
