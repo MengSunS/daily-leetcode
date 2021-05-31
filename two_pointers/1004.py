14406c67dc9bb88486cc64859ed9f3338f22eabf
Pokemonzz <sunx0585@umn.edu>
2021-05-31 13:30:34 -0700

    Max Consecutive Ones III

--- two_pointers/1004.py
+++ two_pointers/1004.py
@@ -1,3 +1,5 @@
+hardest part is the translation: find longest subarray with atMost K 0s.
+
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         res = i = 0
@@ -11,6 +13,21 @@ class Solution:
             res = max(res, j - i + 1)
         return res
     
+# no need to keep tarck of res, instead, j ,i are not the corrspoding index, but the j - i + 1 is the 
+window size. when okay, try to expand j, when exceed, move i, j together, till j hits the end.       
+                
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        res = i = 0
+        n = len(nums)
+        zeros = 0
+        for j in range(n):
+            zeros += (nums[j] == 0)
+            if zeros > k:
+                zeros -= (nums[i] == 0)
+                i += 1
+        return j - i + 1
+    
        
                 
-        
+                
