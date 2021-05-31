2602bbddd4021eb3973d48908500fc01a0f4c3be
Pokemonzz <sunx0585@umn.edu>
2021-05-31 16:46:08 -0700

    Minimum Size Subarray Sum

--- two_pointers/209.py
+++ two_pointers/209.py
@@ -13,4 +13,21 @@ class Solution:
         return res if res != inf else 0
                 
         
+# 209 follow-up: 862. what if the numbers can be negative? monostack
+class Solution:
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
+        dq = collections.deque([(0, -1)])
+        n = len(nums)
+        s = 0
+        res = n + 1
+        for j in range(n):
+            s += nums[j]
+            while dq and dq[0][0] <= s - k:
+                res = min(res, j - dq.popleft()[1])
+            while dq and dq[-1][0] >= s:
+                dq.pop()
+            dq.append((s, j))
+        return res if res != n + 1 else -1
+            
         
+                
