af677b7c264d2874b5825b08cb7e29f716fe05d8
Pokemonzz <sunx0585@umn.edu>
2021-05-31 14:59:11 -0700

    Fruit Into Baskets

---
+++ two_pointers/904.py
@@ -0,0 +1,18 @@
+# translation: longest subarray with atMost 2 distinct integers
+class Solution:
+    def totalFruit(self, tree: List[int]) -> int:
+        res = i = 0
+        n = len(tree)
+        count = {}
+        for j in range(n):
+            count[tree[j]] = count.get(tree[j], 0) + 1
+            while len(count) > 2:
+                count[tree[i]] -= 1
+                if count[tree[i]] == 0:
+                    del count[tree[i]]
+                i += 1
+            res = max(res, j - i + 1)
+        return res
+                
+            
+        
