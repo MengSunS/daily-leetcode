b7cf149f9ca9b138028bf1af1f48d7f36aaa1a01
Pokemonzz <sunx0585@umn.edu>
2021-05-31 17:46:24 -0700

    Replace the Substring for Balanced String

--- two_pointers/1234.py
+++ two_pointers/1234.py
@@ -1,13 +1,19 @@
+# find shortest subarray safities outside the window all count <= n / 4
 class Solution:
     def balancedString(self, s: str) -> int:
-        count = collections.Counter(s)
-        i = 0
         n = len(s)
-        ret = n-1
-        for j, ch in enumerate(s):
-            count[ch] -= 1
-            while i < n and all(count[c] <= n/4 for c in count):
-                ret = min(ret, j-i+1)
-                count[s[i]] += 1
+        i = 0
+        res = n + 1
+        # cnt = {c: 0 for c in 'QWER'}
+        cnt = collections.Counter(s)
+        for j in range(n):
+            cnt[s[j]] -= 1
+            while i < n and all(cnt[c] <= n / 4 for c in 'QWER'):
+                res = min(res, j - i + 1)
+                cnt[s[i]] += 1
                 i += 1
-        return ret
+            # res = min(res, j - i + 2)
+        return res
+    
+    # 注释里的是错误的。这是因为：1）在意的是区间外面的是否可以，而非窗口里面的，判断时需要用外面的；2）while 里面的已经(yi jing)合法的，取个最小的，不要想放在外面
+        
