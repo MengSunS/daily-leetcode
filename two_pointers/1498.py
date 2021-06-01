8e32735368c5e8404ef6144600486fd41070c7cf
Pokemonzz <sunx0585@umn.edu>
2021-06-01 16:18:51 -0700

    Number of Subsequences That Satisfy the Given Sum Condition

--- two_pointers/1498.py
+++ two_pointers/1498.py
@@ -1,4 +1,6 @@
-
+ # j - i 而非j - i - 1的原因是包含从i + 1, i + 2..j之间的，除了min i是必须的，下一步是i += 1,这时不记下来单独的i就错过了；== 和< 公共用else而非单独开两行的原因是：test case可能有重复的数，所以右边固定时，左边下一个也满足
+    
+        
 
 class Solution:
     def numSubseq(self, nums: List[int], target: int) -> int:
