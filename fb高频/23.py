198da394a77765f0825e8b7edf40e23421c44d63
Pokemonzz <sunx0585@umn.edu>
2021-06-15 15:04:01 -0700

    Merge k Sorted Lists

--- fb高频/23.py
+++ fb高频/23.py
@@ -1,10 +1,14 @@
+# Method 1: merge sort, divide & conquer. Best solution of this problem. 
+
+
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 
-# O(nlogk), k is number of lists
+# O(nklogk), k is number of lists, nk is total number of nodes, space is recursion stack O(logk).
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
         if not lists: return None
@@ -27,4 +31,28 @@ class Solution:
         head.next = l or r
         return dummy.next
                 
-        
+
+
+# Method 2, priority queue, time is same with mergesort (divide and conquer), space is O(k), while merge sort space is only O(logk).
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# time complexity: O(nklogk), nk is total number of nodes, k is size of priority queue. 
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        pq = [[l.val, i, l] for i, l in enumerate(lists) if l]
+        heapq.heapify(pq)
+        dummy = head = ListNode(0)
+        while pq:
+            _, i, node = heapq.heappop(pq)
+            head.next = node
+            head = head.next
+            if node.next:
+                heapq.heappush(pq, [node.next.val, i, node.next])
+        return dummy.next
+    
+    
+                
