495ea1a88fe12b0c9d85c16b8fb8899c17e8091c
Pokemonzz <sunx0585@umn.edu>
2021-06-16 23:59:07 -0700

    Count and Say

--- fb高频/38.py
+++ fb高频/38.py
@@ -1,4 +1,6 @@
 # 题意： (i + 1) count and say ith result
+# Method 1: iterative
+
 class Solution:
     def countAndSay(self, n: int) -> str:
         res = '1'
@@ -20,5 +22,22 @@ class Solution:
         return res
         
     
-            
+# Method 2: recursive
+
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return '1'
+        s = self.countAndSay(n - 1)
+        i = 0
+        res = ''
+        while i < len(s):
+            cnt = 1
+            while i + 1 < len(s) and s[i + 1] == s[i]:
+                cnt += 1
+                i += 1
+            res += str(cnt) + s[i]
+            i += 1
+        return res
+                
         
