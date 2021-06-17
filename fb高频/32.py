3ae86d46320ec5dddd4c45e1163d860cb4a2d7c0
Pokemonzz <sunx0585@umn.edu>
2021-06-16 18:36:45 -0700

    Longest Valid Parentheses

--- fb高频/32.py
+++ fb高频/32.py
@@ -1,3 +1,5 @@
+# Method 1: stack
+
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
         stack = [] # index of (, and index of unpaired ) as wall
@@ -19,9 +21,27 @@ class Solution:
                     
         return res
     
-    
-        
+  
 
+# Method 2: DP
+
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        n = len(s)
+        dp = [0] * n
+        for i in range(1, n):
+            if s[i] == ')':
+                if s[i - 1] == '(':
+                    dp[i] = 2 + (dp[i - 2] if i - 2 >= 0 else 0)
+                elif i - dp[i - 1] - 1 >= 0 and s[i - dp[i - 1] - 1] == '(': # dp[i - 1] xxx
+                    dp[i] = 2 + dp[i - 1] + (dp[i - dp[i - 1] - 2] if i - dp[i - 1] - 2 >= 0 else 0) 
+                         
+        return max(dp) if dp else 0
+                    
+                    
+                        
+                
+        
         
       
         
