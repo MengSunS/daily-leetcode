e29752a7a1b390c057ed4f9e3681495fa763d767
Pokemonzz <sunx0585@umn.edu>
2021-06-28 22:26:52 -0700

    Number of Islands

--- fb高频/200.py
+++ fb高频/200.py
@@ -30,4 +30,46 @@ class Solution:
         return res
     
                 
+# Union Find
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m, n = len(grid), len(grid[0])
+        parent = [i for i in range(m * n)]
+        self.cnt = sum(grid[i][j] == '1' for i in range(m) for j in range(n))
+       
+        # def find(x):
+        #     y = x
+        #     while y != parent[y]:
+        #         y = parent[y]
+        #     while x != y:
+        #         px = parent[x]
+        #         parent[x] = y
+        #         x = px
+        #     return y
         
+        def find(x):
+            if x != parent[x]:
+                return find(parent[x])
+            return x
+        
+        def union(x, y):
+            root_x, root_y = find(x), find(y)
+            if root_x != root_y:
+                self.cnt -= 1
+                parent[root_x] = root_y
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '0':
+                    continue
+                index = i * n + j
+                if j < n - 1 and grid[i][j + 1] == '1':
+                    union(index, index + 1)
+                if i < m - 1 and grid[i + 1][j] == '1':
+                    union(index, index + n)
+        
+        return self.cnt
+                    
+            
+          
