f4ff07accf0f5493ed8209b7cd99eacc63ccc269
Pokemonzz <sunx0585@umn.edu>
2021-07-01 18:57:18 -0700

    LCA of BST or normal binary tree

--- fb高频/235.py
+++ fb高频/235.py
@@ -1,3 +1,5 @@
+# LC 235
+
 #注意审题， 说的是BST,利用这个性质
 # 本解法之所有可以直接.right or left是因为BST是 in-order,左右就是下一个更小或更大的
 class Solution:
@@ -20,4 +22,29 @@ class Solution:
             else:
                 return root 
         
+# LC 236
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return 
+    
+        if root == p or root == q:
+            return root
+         
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
         
+        if left and right:
+            return root
+        return left or right
+    
+    
+                
