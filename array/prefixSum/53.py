48e008eda7e6d8417a755d9c6f46766c87c23a48
Pokemonzz <sunx0585@umn.edu>
2021-07-12 22:56:44 -0700

    Maximum Subarray

--- array/prefixSum/53.py
+++ array/prefixSum/53.py
@@ -1,6 +1,3 @@
-# Dp, 不是prefix,dp[i] stands for maximum sum ends with nums[i]
-
-
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         n = len(nums)
@@ -10,13 +7,34 @@ class Solution:
             dp[i] = nums[i] + max(dp[i - 1], 0)
         return max(dp)
     
-    def maxSubArray_better(self, nums: List[int]) -> int:
+    def maxSubArray_DP_better(self, nums: List[int]) -> int:
         n = len(nums)
         curSum = res = nums[0]
         for i in range(1, n):
             curSum = nums[i] + max(curSum, 0)
             res = max(res, curSum)
         return res
+
+    def maxSubArray_prefixSum(self, nums: List[int]) -> int:
+        preSum = 0
+        miniSum = 0
+        res = float('-inf')
+        for i, a in enumerate(nums):
+            preSum += a
+            res = max(res, preSum - miniSum)
+            miniSum = min(miniSum, preSum)
+        return res
+
+    def maxSubArray_greedy(self, nums: List[int]) -> int:
+        res = float('-inf')
+        preSum = 0
+        for a in nums:
+            preSum += a
+            res = max(res, preSum)
+            preSum = max(preSum, 0)
+        return res
+    
+   
     
             
         
