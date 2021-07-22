3f083df79e8c783b7ed9590e688f95eb56837c74
Pokemonzz <sunx0585@umn.edu>
2021-07-22 11:26:04 -0700

    Subarray Sum Equals K

--- fb高频/560.py
+++ fb高频/560.py
@@ -27,4 +27,22 @@ class Solution:
             if cur == k:
                 res += 1
         return res
-            
+
+
+# if all non negatives
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        i, res, cur = 0, 0, 0
+        
+        for j in range(n):
+            cur += nums[j]
+            while cur > k:
+                cur -= nums[i]
+                i += 1
+            while cur == k:
+                res += 1
+                cur -= nums[i]
+                i += 1
+                
+        return res            
