5ed7160e10826c61cb8b77db41090e034702d1bf
Pokemonzz <sunx0585@umn.edu>
2021-07-25 14:22:03 -0700

    Distinct Subsequences II

--- fb高频/940.py
+++ fb高频/940.py
@@ -5,4 +5,20 @@ class Solution:
             end[ord(ch) - ord('a')] = sum(end) + 1
         return sum(end) % (10 ** 9 + 7)
                 
-        
+
+
+
+class Solution:
+    def distinctSubseqII(self, s: str) -> int:
+        s = '#' + s
+        n = len(s)
+        dp = [0] * n
+        dp[0] = 1
+        M = 10 ** 9 + 7
+        last = {}
+        for i in range(1, n):
+            dp[i] = 2 * dp[i - 1] % M
+            if s[i] in seen:
+                dp[i] = (dp[i] - dp[last[s[i]] - 1]) % M
+            last[s[i]] = i
+        return dp[-1] - 1        
