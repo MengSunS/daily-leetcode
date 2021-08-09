e2b9788fb28a533a9341bb2dc43f4e0d2a8bcd61
Pokemonzz <sunx0585@umn.edu>
2021-08-09 13:49:36 -0700

    Flatten Binary Tree to Linked List

--- fb高频/114.py
+++ fb高频/114.py
@@ -1,26 +1,47 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# iterative 
+
+# time O(N), each node at most process twice, space is O(1)
 class Solution:
-    def flatten(self, root: TreeNode) -> None:
+    def flatten(self, root: Optional[TreeNode]) -> None:
         """
         Do not return anything, modify root in-place instead.
         """
-        if not root: return 
-        if not root.left:
-            return self.flatten(root.right)
-        self.flatten(root.left)
-        self.flatten(root.right)
-        h1, h2 = root.left, root.right 
-        root.right = h1
-        root.left = None
-        while h1.right:
-            h1 = h1.right
-        h1.right = h2
+        if not root:
+            return None
         
+        node = root
+        while node:
+            if node.left:
+                rightmost = node.left
+                while rightmost.right:
+                    rightmost = rightmost.right
+                rightmost.right = node.right
+                node.right = node.left
+                node.left = None
+            node = node.right
+
+# recursion
+
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        def flattenTree(node):
+            if not node:
+                return None
+            
+            if not node.left and not node.right:
+                return node
+    
+            l_t = flattenTree(node.left)
+            r_t = flattenTree(node.right)
+            
+            if l_t:
+                l_t.right = node.right
+                node.right = node.left
+                node.left = None
+                
+            return r_t if r_t else l_t #主要错的是helper函数没返回，每个当前 的node需要返回tail node. 只要调用递归有返回，上级递归函数主体必然要返回。这应该归咎于cs基础缺失。
         
-        
-        
+        flattenTree(root)
