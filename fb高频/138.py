bd5069dce1e0a11688118c04fbd3f92f8ab224a7
Pokemonzz <sunx0585@umn.edu>
2021-08-10 17:31:41 -0700

    Copy List with Random Pointer

--- fb高频/138.py
+++ fb高频/138.py
@@ -6,6 +6,33 @@ class Node:
         self.next = next
         self.random = random
 """
+# two pass, 但简单明了 O(2N)
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        if not head: return None
+        clone_map = {}
+        node = head
+        while node:
+            clone_map[node] = Node(node.val)
+            node = node.next
+        
+        for node in clone_map.keys():
+            if node.next:
+                clone_map[node].next = clone_map[node.next]
+            if node.random:
+                clone_map[node].random = clone_map[node.random]
+            
+        return clone_map[head]
+
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
 
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
