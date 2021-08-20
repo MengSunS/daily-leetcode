b624c46ac6135d4101d40a52d8f5430ba7185895
Pokemonzz <sunx0585@umn.edu>
2021-08-19 23:04:30 -0700

    Remove Invalid Parentheses

--- fb高频/301.py
+++ fb高频/301.py
@@ -33,4 +33,45 @@ class Solution:
                 else: r += 1
         return l, r
                     
-        
+#   BFS
+
+
+class Solution:
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+        def validate(s):
+            l, r = 0, 0
+            for a in s:
+                if a == '(':
+                    l += 1
+                elif a == ')':
+                    if l: l -= 1
+                    else: r += 1
+            return [l, r]
+       
+        def bfs(s, l, r):
+            nonlocal Found
+            q = [(s, l, r)]
+            nq = []
+            seen = set()
+            while q:
+                for s, l, r in q:
+                    if l == 0 and r == 0 and validate(s) == [0, 0]:
+                        res.append(s)
+                        Found = True
+                    if Found:
+                        continue
+                    for i in range(len(s)):
+                        if s[i] == '(' and l and s[:i] + s[i + 1:] not in seen:
+                            nq.append((s[:i] + s[i + 1:], l - 1, r))
+                            seen.add(s[:i] + s[i + 1:])
+                        elif s[i] == ')' and r and s[:i] + s[i + 1:] not in seen:
+                            nq.append((s[:i] + s[i + 1:], l, r - 1))
+                            seen.add(s[:i] + s[i + 1:])
+                q, nq = nq, []
+        l, r = validate(s)
+        res = []
+        Found = False
+        bfs(s, l, r)
+        return res
+                        
+                    
