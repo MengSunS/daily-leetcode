2fcf8b5ec9bcf2216235e307937f939887406889
Pokemonzz <sunx0585@umn.edu>
2021-09-12 16:49:34 -0700

    Text Justification

--- fb高频/1026.py
+++ fb高频/1026.py
@@ -1,3 +1,4 @@
+# top down
 class Solution:
     def maxAncestorDiff(self, root: TreeNode) -> int:
         def dfs(node, min_val, max_val):
@@ -12,3 +13,19 @@ class Solution:
         return self.res
     
     # 默默的backtracking 
+
+# bottom up
+
+class Solution:
+    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        self.res = 0
+        def dfs(node):
+            if not node:
+                return float('inf'), float('-inf')
+            l_min, l_max = dfs(node.left)
+            r_min, r_max = dfs(node.right)
+            self.res = max(self.res, node.val - min(l_min, r_min), max(l_max, r_max) - node.val)
+            return min(l_min, r_min, node.val), max(l_max, r_max, node.val)
+        
+        dfs(root)
+        return self.res
--- fb高频/68.py
+++ fb高频/68.py
@@ -1,5 +1,50 @@
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        res = []
+        def getNwords(pos):
+            n = 0
+            acc_len = 0
+            for i in range(pos, len(words)):
+                acc_len += len(words[i])
+                n += 1
+                if acc_len + (n - 1) > maxWidth:
+                    n -= 1
+                    acc_len -= len(words[i])
+                    break 
+            return n, acc_len
+        
+        def getLine(pos, n, acc_len):
+            if n == 1 or pos + n - 1== len(words) - 1:
+                l = ' '.join(words[pos:pos + n])
+                line = l + ' ' * (maxWidth - len(l))
+            else:
+                d, extra = (maxWidth - acc_len) // (n - 1), (maxWidth - acc_len) % (n - 1)
+                line = ''
+                for i in range(pos, pos + n):
+                    line += words[i]
+                    if i != pos + n - 1:
+                        line += ' ' * d
+                    if extra:
+                        line += ' '
+                        extra -= 1
+            return line
+        
+        def dfs(pos):
+            if pos == len(words):
+                return 
+            n, acc_len = getNwords(pos)
+            line = getLine(pos, n, acc_len)
+            res.append(line)
+            dfs(pos + n)
+            
+            
+        dfs(0)
+        return res
+            
+                
+       
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
         def getKWords(i):
             k = 0
             l = ' '.join(words[i:i+k])
