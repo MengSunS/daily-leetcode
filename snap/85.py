fa17ce4bcf5b42170b924bd426e85edc94b011d0
Pokemonzz <sunx0585@umn.edu>
2021-09-16 22:20:20 -0700

    Maximal Rectangle

---
+++ snap/85.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        if not matrix or not matrix[0]: return 0
+        n = len(matrix[0])
+        heights = [0] * (n + 1)
+        ans = 0
+        for row in matrix:
+            for i in range(n):
+                heights[i] = heights[i] + 1 if row[i] == '1' else 0
+            stack = []
+            for i, h in enumerate(heights):
+                while stack and h <= heights[stack[-1]]:
+                    height = heights[stack.pop()]
+                    width = i if not stack else i - stack[-1] - 1
+                    ans = max(ans, height * width)
+                stack.append(i)
+        return ans
