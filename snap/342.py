2bc6d97ca760333e4e74c5505c73a02ed3ea430c
Pokemonzz <sunx0585@umn.edu>
2021-09-25 09:45:28 -0700

    Power of Four

--- snap/342.py
+++ snap/342.py
@@ -1,4 +1,22 @@
 class Solution:
     def isPowerOfFour(self, num: int) -> bool:
         return num > 0 and num & (num-1) == 0 and 0b1010101010101010101010101010101 & num == num   
-        
+
+
+    def isPowerOfFour(self, num: int) -> bool:
+        if num < 0 : return False
+        def helper(num):
+            if num < 4:
+                if num == 1:
+                    return True
+                else:
+                    return False
+            return helper(num/4)
+        return helper(num)
+    
+    def isPowerOfFour(self, num: int) -> bool:
+        if num < 0 : return False
+        while num >= 4:
+            num /= 4
+        return num == 1
+                    
