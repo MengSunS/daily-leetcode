c05b20752c96278a6e95ea3f9c238de9a1543210
Pokemonzz <sunx0585@umn.edu>
2021-10-07 12:05:27 -0700

    Read N Characters Given Read4 II - Call multiple times

---
+++ snap/158.py
@@ -0,0 +1,42 @@
+class Solution:
+    def __init__(self):
+        self.q = deque()
+        
+        
+    def read(self, buf: List[str], n: int) -> int:
+        i = 0
+        while i < n:
+            if self.q:
+                buf[i] = self.q.popleft()
+                i += 1
+            else:
+                buf4 = [''] * 4
+                num = read4(buf4)
+                if num == 0:
+                    break
+                self.q.extend(buf4[:num])
+        return i
+
+
+# 157
+
+class Solution:
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Number of characters to read (int)
+        :rtype: The number of actual characters read (int)
+        """
+        q = deque()
+        i = 0
+        while i < n:
+            if q:
+                buf[i] = q.popleft()
+                i += 1
+            else:
+                buf4 = [''] * 4
+                num =  read4(buf4)
+                if num == 0:
+                    break
+                q.extend(buf4[:num])
+        return i
