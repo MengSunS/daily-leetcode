176428ab473ac02dd28b33e40ebe9a568324dd3a
Pokemonzz <sunx0585@umn.edu>
2021-10-07 12:34:14 -0700

    One Edit Distance

--- snap/161.py
+++ snap/161.py
@@ -1,4 +1,17 @@
-# O(N)
+class Solution:
+    def isOneEditDistance(self, s: str, t: str) -> bool:
+        m, n = len(s), len(t)
+        if s == t: return False
+        i, j = 0, 0
+        while i < m and j < n and s[i] == t[j]:
+            i += 1
+            j += 1
+        if m > n: return s[i + 1:] == t[j:]
+        elif m < n: return s[i:] == t[j+1:]
+        else: return s[i+1:] == t[j+1:]
+      
+
+  # O(N)
 class Solution:
     def isOneEditDistance(self, s: str, t: str) -> bool:
         if len(s) > len(t):
