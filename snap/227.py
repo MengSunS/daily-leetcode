20cf8c141dbfb00130157141493308d18fc46099
Pokemonzz <sunx0585@umn.edu>
2021-10-08 10:13:13 -0700

    Basic Calculator II

--- snap/227.py
+++ snap/227.py
@@ -24,4 +24,34 @@ class Solution:
                         
                         
                     
-        
+# O(1) space, use res and prev, when current s[i] in {'+', '-'}, update prev into res
+
+class Solution:
+    def calculate(self, s: str) -> int:
+        res, prev, num, sign = 0, 0, 0, '+'
+        for i, a in enumerate(s):
+            if a.isdigit():
+                num = num * 10 + int(a)
+            if i == len(s) - 1 or a in {'+', '-', '*', '/'}:
+                if sign == '+':
+                    prev = num
+                elif sign == '-':
+                    prev = -num
+                elif sign == '*':
+                    prev *= num
+                else:
+                    if prev / num >= 0:
+                        prev //= num
+                    else:
+                        prev = math.ceil(prev / num)
+                if a in {'+', '-'}: #关键是这行
+                    res += prev
+                    prev = 0    
+                sign, num = a, 0
+            else:
+                continue
+        return res + prev
+                        
+                        
+                    
+                
