60eacfd80c61fa33652edff942a0f93d633184cc
Pokemonzz <sunx0585@umn.edu>
2021-10-08 11:35:42 -0700

    Basic Calculator III

--- snap/772.py
+++ snap/772.py
@@ -1,5 +1,35 @@
 class Solution:
     def calculate(self, s: str) -> int:
+        s += '#'
+        def helper(stack, i):
+            num, sign = 0, '+'
+            while i < len(s):
+                if s[i].isdigit():
+                    num = num * 10 + int(s[i])
+                    i += 1
+                elif s[i] == ' ':
+                    i += 1
+                elif s[i] == '(':
+                    num, i = helper([], i + 1)
+                else:
+                    if sign == '+':
+                        stack.append(num)
+                    elif sign == '-':
+                        stack.append(-num)
+                    elif sign == '*':
+                        stack.append(stack.pop() * num)
+                    else:
+                        stack.append(int(stack.pop() / num))
+                    if s[i] == ')':
+                        return sum(stack), i + 1
+                    else:
+                        sign, num = s[i], 0
+                        i += 1
+            return sum(stack)
+        return helper([], 0)
+            
+        class Solution:
+    def calculate(self, s: str) -> int:
         s = s + '#'
         def helper(stack, i):
             num, sign = 0, '+'
